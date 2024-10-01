a44e09b669417da594b7ce98ce5b0f44970d8e8f
kevin cuadros <kevindavidcuadros@gmail.com>
2024-09-30 22:08:21 -0500

    Custom modifiers

--- Project-3-SwiftUI-view-modifiers/ContentView.swift
+++ Project-3-SwiftUI-view-modifiers/ContentView.swift
@@ -25,6 +25,15 @@ struct ContentView: View {
     
     var body: some View {
         
+        Color.blue
+            .frame(width: 300, height: 200)
+            .watermark(with: "Hacking with Swift")
+        
+        Text("Modifiers Extension")
+            .titleStyle()
+        
+        Text("Text with modifiers")
+            .modifier(TitleModifiers())
         
         CapsuleText(text: "Kevin Cuadros")
         
@@ -82,6 +91,50 @@ struct CapsuleText: View {
             .clipShape(.capsule)
     }
 }
+
+
+struct TitleModifiers: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundStyle(.white)
+            .padding()
+            .background(.red)
+            .clipShape(.rect(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(TitleModifiers())
+    }
+}
+
+// ---------------------- //
+
+struct Watermark: ViewModifier {
+    
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundStyle(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+    
+}
+
+extension View {
+    func watermark(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
 
 #Preview {
     ContentView()
